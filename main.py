44c65317897cb07a5dd42a787976ff50c4339b65
empicosch5 <es17072013@gmail.com>
2025-05-24 14:06:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import telebot
+from bot_logic import gen_pass
+from coinflip import coin1
 
-# Инициализация бота с использованием его токена
-bot = telebot.TeleBot('djakdjaksda")
 
-# Обработчик команды '/start' и '/hello'
-@bot.message_handler(commands=['start', 'hello'])
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7896634459:AAEzdxiT-uwTxx4AHl_SJB4I7boaMApJkHM")
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
 
-# Обработчик команды '/heh'
-@bot.message_handler(commands=['heh'])
-def send_heh(message):
-    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
-    bot.reply_to(message, "he" * count_heh)
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
 
-# Запуск бота
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['gen_pass'])
+def send_pas(message):
+    bot.reply_to(message, gen_pass(8))
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    bot.reply_to(message, coin1())
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
 bot.polling()
